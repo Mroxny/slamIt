81cc4a47c036036b6ac1bb1ec2c33f830d3562f7
Mroxny <maciej.h.mrozek@gmail.com>
2025-09-19 10:33:12 +0200

    Update repository (temporary)

--- backend/internal/repository/user.go
+++ backend/internal/repository/user.go
@@ -3,43 +3,43 @@ package repository
 import (
 	"errors"
 
-	"github.com/Mroxny/slamIt/internal/model"
+	"github.com/Mroxny/slamIt/internal/api"
 )
 
 type UserRepository struct {
-	users  []model.User
+	users  []api.User
 	nextID int
 }
 
 func NewUserRepository() *UserRepository {
 	return &UserRepository{
-		users:  []model.User{},
+		users:  []api.User{},
 		nextID: 1,
 	}
 }
 
-func (r *UserRepository) GetAll() []model.User {
+func (r *UserRepository) GetAll() []api.User {
 	return r.users
 }
 
-func (r *UserRepository) GetByID(id string) (*model.User, error) {
+func (r *UserRepository) GetByID(id string) (*api.User, error) {
 	for _, u := range r.users {
-		if u.ID == id {
+		if *u.Id == id {
 			return &u, nil
 		}
 	}
 	return nil, errors.New("user not found")
 }
 
-func (r *UserRepository) Create(user *model.User) (*model.User, error) {
+func (r *UserRepository) Create(user *api.User) (*api.User, error) {
 	r.users = append(r.users, *user)
 	return user, nil
 }
 
-func (r *UserRepository) Update(id string, updated model.User) (*model.User, error) {
+func (r *UserRepository) Update(id string, updated api.User) (*api.User, error) {
 	for i, u := range r.users {
-		if u.ID == id {
-			if updated.Name == "" || updated.Email == "" {
+		if *u.Id == id {
+			if *updated.Name == "" || *updated.Email == "" {
 				return nil, errors.New("invalid input")
 			}
 			r.users[i].Name = updated.Name
@@ -52,7 +52,7 @@ func (r *UserRepository) Update(id string, updated model.User) (*model.User, err
 
 func (r *UserRepository) Delete(id string) error {
 	for i, u := range r.users {
-		if u.ID == id {
+		if *u.Id == id {
 			r.users = append(r.users[:i], r.users[i+1:]...)
 			return nil
 		}
@@ -60,9 +60,9 @@ func (r *UserRepository) Delete(id string) error {
 	return errors.New("user not found")
 }
 
-func (r *UserRepository) GetByEmail(email string) (*model.User, error) {
+func (r *UserRepository) GetByEmail(email string) (*api.User, error) {
 	for _, user := range r.users {
-		if user.Email == email {
+		if *user.Email == email {
 			return &user, nil
 		}
 	}
