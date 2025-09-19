b66042057dbfebd0b76d3177e323d3b86faa2ca1
Mroxny <maciej.h.mrozek@gmail.com>
2025-09-19 20:05:12 +0200

    Additional validation

--- backend/internal/handler/slam_participation.go
+++ backend/internal/handler/slam_participation.go
@@ -23,11 +23,21 @@ func (s *Server) DeleteParticipationUsersUserIDSlamsSlamID(w http.ResponseWriter
 }
 
 func (s *Server) GetParticipationUsersUserIDSlams(w http.ResponseWriter, r *http.Request, userID string) {
-	slams, _ := s.partService.GetSlamsForUser(userID)
+	slams, err := s.partService.GetSlamsForUser(userID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(slams)
 }
 
 func (s *Server) GetParticipationSlamsSlamIDUsers(w http.ResponseWriter, r *http.Request, slamID int) {
-	users, _ := s.partService.GetUsersForSlam(slamID)
+	users, err := s.partService.GetUsersForSlam(slamID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(users)
 }
