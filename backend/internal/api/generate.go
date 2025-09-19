651ce66dc9dcdfe902c85b83e685e8c25b13b752
Mroxny <maciej.h.mrozek@gmail.com>
2025-09-19 10:36:58 +0200

    Implement generated api

---
+++ backend/internal/api/generate.go
@@ -0,0 +1,4 @@
+package api
+
+//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen -config cfg.server.yaml ../../openapi.yaml
+//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen -config cfg.types.yaml ../../openapi.yaml
