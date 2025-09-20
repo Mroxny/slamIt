fd1ef5cddb673f230c0c169adbffb30e4cdcbea0
Mroxny <maciej.h.mrozek@gmail.com>
2025-09-20 13:38:36 +0200

    Update auth util

--- backend/internal/utils/auth.go
+++ backend/internal/utils/auth.go
@@ -3,9 +3,12 @@ package utils
 import (
 	"context"
 	"errors"
+	"fmt"
 	"net/http"
+	"strings"
 	"time"
 
+	"github.com/Mroxny/slamIt/internal/config"
 	"github.com/getkin/kin-openapi/openapi3"
 	"github.com/getkin/kin-openapi/openapi3filter"
 	"github.com/golang-jwt/jwt/v5"
@@ -13,7 +16,22 @@ import (
 	middleware "github.com/oapi-codegen/nethttp-middleware"
 )
 
-var jwtKey = []byte("supersecretkey") // move to env
+type contextKey string
+
+const JWTClaimsContextKey contextKey = "userID"
+
+var (
+	ErrNoAuthHeader      = errors.New("authorization header is missing")
+	ErrInvalidAuthHeader = errors.New("authorization header is malformed")
+	ErrClaimsInvalid     = errors.New("provided claims do not match expected scopes")
+)
+
+var jwtKey = getJwtKey()
+
+func getJwtKey() []byte {
+	cfg, _ := config.GetConfig()
+	return []byte(cfg.JWT.Key)
+}
 
 func GenerateJWT(userID string) (string, error) {
 	claims := &jwt.RegisteredClaims{
@@ -37,27 +55,38 @@ func ValidateJWT(tokenStr string) (string, error) {
 	return claims.Subject, nil
 }
 
+func GetJWSFromRequest(req *http.Request) (string, error) {
+	authHdr := req.Header.Get("Authorization")
+	if authHdr == "" {
+		return "", ErrNoAuthHeader
+	}
+	prefix := "Bearer "
+	if !strings.HasPrefix(authHdr, prefix) {
+		return "", ErrInvalidAuthHeader
+	}
+	return strings.TrimPrefix(authHdr, prefix), nil
+}
+
 func AuthMiddleware(spec *openapi3.T) func(http.Handler) http.Handler {
 	// spec.Servers = nil
 	return middleware.OapiRequestValidatorWithOptions(spec, &middleware.Options{
 		SilenceServersWarning: true,
 		Options: openapi3filter.Options{
 			AuthenticationFunc: func(ctx context.Context, ai *openapi3filter.AuthenticationInput) error {
-				if ai.SecuritySchemeName == "BearerAuth" {
-
+				if ai.SecuritySchemeName == "bearerAuth" {
 					req := ai.RequestValidationInput.Request
-					authHdr := req.Header.Get("Authorization")
-
-					if authHdr == "" {
-						return errors.New("authorization header is missing")
+					jws, err := GetJWSFromRequest(req)
+					if err != nil {
+						return fmt.Errorf("getting JWS: %w", err)
 					}
 
-					// TODO: Validate token
-					if false {
-						return nil
+					userId, err := ValidateJWT(jws)
+					if err != nil {
+						return fmt.Errorf("validating JWS: %w", err)
 					}
 
-					return errors.New("invalid token")
+					newCtx := context.WithValue(req.Context(), JWTClaimsContextKey, userId)
+					*req = *req.WithContext(newCtx)
 				}
 				return nil
 			},
