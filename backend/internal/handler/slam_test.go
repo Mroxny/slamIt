e63c3bb941fdc0e8d590022efb96150e7bcf59fe
Mroxny <maciej.h.mrozek@gmail.com>
2025-09-23 10:34:02 +0200

    Bring back tests

--- backend/internal/handler/slam_test.go
+++ backend/internal/handler/slam_test.go
@@ -1,77 +1,95 @@
 package handler_test
 
-// func TestSlamHandler(t *testing.T) {
-// 	r := router.SetupTestRouter()
-// 	_, token := utils.GetAuthToken(r, "bob@example.com", "P@ssw0rd", false)
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
 
-// 	tests := []struct {
-// 		name     string
-// 		method   string
-// 		url      string
-// 		body     string
-// 		auth     bool
-// 		wantCode int
-// 	}{
-// 		{
-// 			name:     "create valid slam",
-// 			method:   "POST",
-// 			url:      "/slams/",
-// 			body:     `{"title":"Test Slam","description":"Testing slam endpoint"}`,
-// 			auth:     true,
-// 			wantCode: http.StatusCreated,
-// 		},
-// 		{
-// 			name:     "create invalid slam (missing title)",
-// 			method:   "POST",
-// 			url:      "/slams/",
-// 			body:     `{"description":"Charlie"}`,
-// 			auth:     true,
-// 			wantCode: http.StatusBadRequest,
-// 		},
-// 		{
-// 			name:     "get existing slam",
-// 			method:   "GET",
-// 			url:      "/slams/1",
-// 			auth:     true,
-// 			wantCode: http.StatusOK,
-// 		},
-// 		{
-// 			name:     "get non-existing slam",
-// 			method:   "GET",
-// 			url:      "/slams/999999999",
-// 			auth:     true,
-// 			wantCode: http.StatusNotFound,
-// 		},
-// 		{
-// 			name:     "update slam valid",
-// 			method:   "PUT",
-// 			url:      "/slams/1",
-// 			body:     `{"title":"Slam Updated","description":"Updated description"}`,
-// 			auth:     true,
-// 			wantCode: http.StatusOK,
-// 		},
-// 		{
-// 			name:     "delete slam valid",
-// 			method:   "DELETE",
-// 			url:      "/slams/1",
-// 			auth:     true,
-// 			wantCode: http.StatusNoContent,
-// 		},
-// 	}
+	"github.com/Mroxny/slamIt/internal/api"
+	"github.com/Mroxny/slamIt/internal/router"
+	"github.com/Mroxny/slamIt/internal/utils"
+)
 
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			req := httptest.NewRequest(tt.method, tt.url, strings.NewReader(tt.body))
-// 			req.Header.Set("Content-Type", "application/json")
-// 			if tt.auth {
-// 				req.Header.Set("Authorization", "Bearer "+token)
-// 			}
-// 			w := httptest.NewRecorder()
-// 			r.ServeHTTP(w, req)
+func TestSlamHandler(t *testing.T) {
+	r := router.SetupTestRouter()
+	_, token := utils.GetAuthToken(r, "bob@example.com", "P@ssw0rd", false)
+	slamId := "3"
 
-// 			if w.Code != tt.wantCode {
-// 				t.Errorf("%s: got %d, want %d", tt.name, w.Code, tt.wantCode)
-// 			}
-// 		})
-// 	}
-// }
+	tests := []struct {
+		name     string
+		method   string
+		url      string
+		body     string
+		auth     bool
+		wantCode int
+	}{
+		{
+			name:     "create valid slam",
+			method:   "POST",
+			url:      "/slams",
+			body:     `{"title":"Test Slam","description":"Testing slam endpoint", "public":true}`,
+			auth:     true,
+			wantCode: http.StatusCreated,
+		},
+		{
+			name:     "create invalid slam (missing title)",
+			method:   "POST",
+			url:      "/slams",
+			body:     `{"description":"Charlie"}`,
+			auth:     true,
+			wantCode: http.StatusBadRequest,
+		},
+		{
+			name:     "get all public slams",
+			method:   "GET",
+			url:      "/slams",
+			wantCode: http.StatusOK,
+		},
+		{
+			name:     "get existing slam",
+			method:   "GET",
+			url:      "/slams/" + slamId,
+			auth:     true,
+			wantCode: http.StatusOK,
+		},
+		{
+			name:     "get non-existing slam",
+			method:   "GET",
+			url:      "/slams/999999999",
+			auth:     true,
+			wantCode: http.StatusNotFound,
+		},
+		{
+			name:     "update slam valid",
+			method:   "PUT",
+			url:      "/slams/" + slamId,
+			body:     `{"title":"Slam Updated","description":"Updated description", "public":false}`,
+			auth:     true,
+			wantCode: http.StatusOK,
+		},
+		{
+			name:     "delete slam valid",
+			method:   "DELETE",
+			url:      "/slams/" + slamId,
+			auth:     true,
+			wantCode: http.StatusNoContent,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			req := httptest.NewRequest(tt.method, api.ServerUrlDev+tt.url, strings.NewReader(tt.body))
+			req.Header.Set("Content-Type", "application/json")
+			if tt.auth {
+				req.Header.Set("Authorization", "Bearer "+token)
+			}
+			w := httptest.NewRecorder()
+			r.ServeHTTP(w, req)
+
+			if w.Code != tt.wantCode {
+				t.Errorf("%s: got %d, want %d, msg: %s", tt.name, w.Code, tt.wantCode, w.Body)
+			}
+		})
+	}
+}
--- backend/internal/handler/user_test.go
+++ backend/internal/handler/user_test.go
@@ -1,68 +1,79 @@
 package handler_test
 
-// func TestUserHandler(t *testing.T) {
-// 	r := router.SetupTestRouter()
-// 	uId, token := utils.GetAuthToken(r, "bob@example.com", "P@ssw0rd", false)
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
 
-// 	tests := []struct {
-// 		name     string
-// 		method   string
-// 		url      string
-// 		body     string
-// 		auth     bool
-// 		wantCode int
-// 	}{
-// 		{
-// 			name:     "get all users",
-// 			method:   "GET",
-// 			url:      "/users/",
-// 			auth:     true,
-// 			wantCode: http.StatusOK,
-// 		},
-// 		{
-// 			name:     "get existing user",
-// 			method:   "GET",
-// 			url:      "/users/" + uId,
-// 			auth:     true,
-// 			wantCode: http.StatusOK,
-// 		},
-// 		{
-// 			name:     "get non-existing user",
-// 			method:   "GET",
-// 			url:      "/users/xxx",
-// 			auth:     true,
-// 			wantCode: http.StatusNotFound,
-// 		},
-// 		{
-// 			name:     "update user valid",
-// 			method:   "PUT",
-// 			url:      "/users/" + uId,
-// 			body:     `{"name":"Alice Updated","email":"alice@new.com"}`,
-// 			auth:     true,
-// 			wantCode: http.StatusOK,
-// 		},
-// 		{
-// 			name:     "delete user valid",
-// 			method:   "DELETE",
-// 			url:      "/users/" + uId,
-// 			auth:     true,
-// 			wantCode: http.StatusNoContent,
-// 		},
-// 	}
+	"github.com/Mroxny/slamIt/internal/api"
+	"github.com/Mroxny/slamIt/internal/router"
+	"github.com/Mroxny/slamIt/internal/utils"
+)
 
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			req := httptest.NewRequest(tt.method, tt.url, strings.NewReader(tt.body))
-// 			req.Header.Set("Content-Type", "application/json")
-// 			if tt.auth {
-// 				req.Header.Set("Authorization", "Bearer "+token)
-// 			}
-// 			w := httptest.NewRecorder()
-// 			r.ServeHTTP(w, req)
+func TestUserHandler(t *testing.T) {
+	r := router.SetupTestRouter()
+	uId, token := utils.GetAuthToken(r, "bob@example.com", "P@ssw0rd", false)
 
-// 			if w.Code != tt.wantCode {
-// 				t.Errorf("%s: got %d, want %d", tt.name, w.Code, tt.wantCode)
-// 			}
-// 		})
-// 	}
-// }
+	tests := []struct {
+		name     string
+		method   string
+		url      string
+		body     string
+		auth     bool
+		wantCode int
+	}{
+		{
+			name:     "get all users",
+			method:   "GET",
+			url:      "/users",
+			auth:     true,
+			wantCode: http.StatusOK,
+		},
+		{
+			name:     "get existing user",
+			method:   "GET",
+			url:      "/users/" + uId,
+			auth:     true,
+			wantCode: http.StatusOK,
+		},
+		{
+			name:     "get non-existing user",
+			method:   "GET",
+			url:      "/users/xxx",
+			auth:     true,
+			wantCode: http.StatusNotFound,
+		},
+		{
+			name:     "update user valid",
+			method:   "PUT",
+			url:      "/users/" + uId,
+			body:     `{"name":"Alice Updated","email":"alice@new.com"}`,
+			auth:     true,
+			wantCode: http.StatusOK,
+		},
+		{
+			name:     "delete user valid",
+			method:   "DELETE",
+			url:      "/users/" + uId,
+			auth:     true,
+			wantCode: http.StatusNoContent,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			req := httptest.NewRequest(tt.method, api.ServerUrlDev+tt.url, strings.NewReader(tt.body))
+			req.Header.Set("Content-Type", "application/json")
+			if tt.auth {
+				req.Header.Set("Authorization", "Bearer "+token)
+			}
+			w := httptest.NewRecorder()
+			r.ServeHTTP(w, req)
+
+			if w.Code != tt.wantCode {
+				t.Errorf("%s: got %d, want %d, msg: %s", tt.name, w.Code, tt.wantCode, w.Body)
+			}
+		})
+	}
+}
